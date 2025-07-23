cef4899346680d589205f247f472cedc3f22266a
Saullo G. P. Castro <saullogiovani@gmail.com>
2025-07-23 11:20:56 +0200

    DEV: internal forces

--- deriving_equations/quad4r/derive_quad4r_fint.py
+++ deriving_equations/quad4r/derive_quad4r_fint.py
@@ -10,7 +10,7 @@ Constitutive stiffness matrix (linear) for Quad4R element
 """
 import numpy as np
 import sympy
-from sympy import simplify, Matrix, var
+from sympy import simplify, Matrix, var, symbols
 from sympy.vector import CoordSys3D
 
 
@@ -345,20 +345,6 @@ for node_i in range(NUM_NODES):
     print(node_i*DOF + 5)
     KC0e[node_i*DOF + 5, node_i*DOF + 5] = K6ROT
 
-# KC0 represents the global linear stiffness matrix
-print()
-print('transformation local to global')
-var('r11, r12, r13, r21, r22, r23, r31, r32, r33')
-Rlocal2global = Matrix([[r11, r12, r13],
-                        [r21, r22, r23],
-                        [r31, r32, r33]])
-R = sympy.zeros(NUM_NODES*DOF, NUM_NODES*DOF)
-for i in range(2*NUM_NODES):
-    R[i*DOF//2:(i+1)*DOF//2, i*DOF//2:(i+1)*DOF//2] += Rlocal2global
-
-#NOTE line below to visually check the Rmatrix
-#np.savetxt('Rmatrix.txt', R, fmt='% 3s')
-
 nonzero = set()
 for ind, val in np.ndenumerate(KC0e):
     if sympy.expand(val) == 0:
@@ -385,7 +371,39 @@ for i in range(NUM_NODES*DOF):
     rows.append(cols)
 KC0e = Matrix(rows)
 
-KC0 = R*KC0e*R.T
+# displacements in global coordinates corresponding to one finite element
+ue = Matrix([symbols(r'ue[%d]' % i) for i in range(0, BL.shape[1])])
+
+finte = KC0e*ue
+
+print('transformation local to global')
+var('r11, r12, r13, r21, r22, r23, r31, r32, r33')
+Rlocal2global = Matrix([[r11, r12, r13],
+                        [r21, r22, r23],
+                        [r31, r32, r33]])
+R = sympy.zeros(NUM_NODES*DOF, NUM_NODES*DOF)
+for i in range(2*NUM_NODES):
+    R[i*DOF//2:(i+1)*DOF//2, i*DOF//2:(i+1)*DOF//2] += Rlocal2global
+
+nonzero = set()
+for ind, val in np.ndenumerate(finte):
+    if sympy.expand(val) == 0:
+        continue
+    i, j = ind
+    name = 'finte[%d]' % (i)
+    nonzero.add(name)
+    print('%s = %s' % (name, simplify(val)))
+
+rows = []
+for i in range(NUM_NODES*DOF):
+    name = 'finte[%d]' % (i)
+    if name in nonzero:
+        rows.append(var(name))
+    else:
+        rows.append(0)
+finte = Matrix(rows)
+fint = R*finte
+
 
 def name_ind(i):
     if i >= 0*DOF and i < 1*DOF:
@@ -403,28 +421,15 @@ print()
 print()
 print('_______________________________________')
 print()
-print('printing code for sparse implementation')
+print('printing fint')
 print('_______________________________________')
 print()
 print()
-KC0_SPARSE_SIZE = 0
-for ind, val in np.ndenumerate(KC0):
-    if sympy.expand(val) == 0:
-        continue
-    KC0_SPARSE_SIZE += 1
+for ind, val in np.ndenumerate(fint):
     i, j = ind
     si = name_ind(i)
-    sj = name_ind(j)
-    print('        k += 1')
-    print('        KC0r[k] = %d+%s' % (i%DOF, si))
-    print('        KC0c[k] = %d+%s' % (j%DOF, sj))
-print('KC0_SPARSE_SIZE', KC0_SPARSE_SIZE)
-print()
-print()
-for ind, val in np.ndenumerate(KC0):
     if sympy.expand(val) == 0:
         continue
-    print('        k += 1')
-    print('        KC0v[k] +=', val)
+    print('            fint[%d+self.%s] +=' % (i%DOF, si), val)
 print()
 print()
