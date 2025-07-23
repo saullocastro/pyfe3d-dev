0ffa8f0774d11b9e3d5ea7184d4e521c94e76dd3
Saullo G. P. Castro <saullogiovani@gmail.com>
2025-07-23 12:41:11 +0200

    DEV: internal forces

--- deriving_equations/spring/derive_spring_KC0_sparse_fint.py
+++ deriving_equations/spring/derive_spring_KC0_sparse_fint.py
@@ -1,7 +1,6 @@
 import numpy as np
 import sympy
-from sympy import simplify, integrate, Matrix, var
-from sympy.vector import CoordSys3D, cross
+from sympy import simplify, Matrix, var, symbols
 
 r"""
 
@@ -17,9 +16,8 @@ r"""
 """
 
 DOF = 6
-num_nodes = 2
+NUM_NODES = 2
 
-var('xi', real=True)
 sympy.var('kxe, kye, kze, krxe, krye, krze', real=True, positive=True)
 
 KC0e = Matrix([
@@ -42,11 +40,16 @@ var('r11, r12, r13, r21, r22, r23, r31, r32, r33')
 Rlocal2global = Matrix([[r11, r12, r13],
                         [r21, r22, r23],
                         [r31, r32, r33]])
-R = sympy.zeros(num_nodes*DOF, num_nodes*DOF)
-for i in range(2*num_nodes):
+R = sympy.zeros(NUM_NODES*DOF, NUM_NODES*DOF)
+for i in range(2*NUM_NODES):
     R[i*DOF//2:(i+1)*DOF//2, i*DOF//2:(i+1)*DOF//2] += Rlocal2global
 
 KC0 = R*KC0e*R.T
+
+# displacements in global coordinates corresponding to one finite element
+ue = Matrix([symbols(r'ue[%d]' % i) for i in range(0, KC0e.shape[1])])
+
+finte = KC0e*ue
 
 def name_ind(i):
     if i >= 0*DOF and i < 1*DOF:
@@ -83,5 +86,41 @@ for ind, val in np.ndenumerate(KC0):
         continue
     print('            k += 1')
     print('            KC0v[k] +=', val)
+print()
+print()
+
+nonzero = set()
+for ind, val in np.ndenumerate(finte):
+    if sympy.expand(val) == 0:
+        continue
+    i, j = ind
+    name = 'finte[%d]' % (i)
+    nonzero.add(name)
+    print('%s = %s' % (name, simplify(val)))
+
+rows = []
+for i in range(NUM_NODES*DOF):
+    name = 'finte[%d]' % (i)
+    if name in nonzero:
+        rows.append(var(name))
+    else:
+        rows.append(0)
+finte = Matrix(rows)
+fint = R*finte
+
+print()
+print()
+print('_______________________________________')
+print()
+print('printing fint')
+print('_______________________________________')
+print()
+print()
+for ind, val in np.ndenumerate(fint):
+    i, j = ind
+    si = name_ind(i)
+    if sympy.expand(val) == 0:
+        continue
+    print('            fint[%d+self.%s] +=' % (i%DOF, si), val)
 print()
 print()
